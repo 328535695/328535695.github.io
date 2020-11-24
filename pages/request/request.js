1bc639b7ba6a444299c895a886a04ecd51407383
unknown <unknown@example.com>
2020-11-24 22:38:22 +0800

    Initial Commit

---
+++ pages/request/request.js
@@ -0,0 +1,70 @@
+// pages/request/request.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    newsList:[
+      {url:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2239550546,2639894896&fm=26&gp=0.jpg",title:"标题3",time:"2020-11-24"},
+      {url:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3651442269,668255224&fm=26&gp=0.jpg",title:"标题2",time:"2020-11-22"},
+      {url:"https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3826551437,2160889623&fm=26&gp=0.jpg",title:"标题1",time:"2020-11-20"}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
